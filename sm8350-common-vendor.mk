e5f887bc85105ff4f16bbba9405034355790c737
1xtAsh <ashraykrsingh321@gmail.com>
2025-10-26 14:47:18 +0530

    sm8350-common: Use MIUI poweroff charging animation

--- sm8350-common-vendor.mk
+++ sm8350-common-vendor.mk
@@ -544,6 +544,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.diag.hal.service.xml \
     vendor.qti.gnss@4.0-service.xml \
     vendor.xiaomi.hardware.citsensorservice@1.1-service.xml \
+    chargeonlymode \
     adpl \
     adsprpcd \
     cdsprpcd \
