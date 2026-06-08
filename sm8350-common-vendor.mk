81cedec6fc30e269168a476af87e7ecab08bc448
Never-Alive <livinglucifer0@gmail.com>
2026-06-08 07:20:39 +0000

    sm8350-common: Decommonize libQnnHtpStub
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>
    Signed-off-by: THE-EGO-999-GT <theego999.me@gmail.com>

--- sm8350-common-vendor.mk
+++ sm8350-common-vendor.mk
@@ -298,7 +298,6 @@ PRODUCT_PACKAGES += \
     libQSEEComAPI \
     libQTEEConnector_vendor \
     libQnnHtpAltPrepStub \
-    libQnnHtpStub \
     libadsp_default_listener \
     libadsprpc \
     libaoa \
