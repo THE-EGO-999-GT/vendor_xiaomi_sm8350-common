b0f4a4f1a879ca39c56edd0dc4557bcd1517d80d
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-09-11 19:30:54 +0000

    sm8350-common: Sync with common tree
    [1]: sm8350-common: Enable CameraX extensions support
    
    Change-Id: I48762295d5279ab06d5d59cf0ec503ae40d8e231
    Signed-off-by: THE-EGO-999-GT <theego999.me@gmail.com>

--- sm8350-common-vendor.mk
+++ sm8350-common-vendor.mk
@@ -533,6 +533,7 @@ PRODUCT_PACKAGES += \
     WfdCommon \
     tcmclient \
     audiosphere \
+    camerax-vendor-extensions \
     com.android.hotwordenrollment.common.util \
     com.qti.dpmframework \
     dpmapi \
